77e2ed5f19c4b969ffb89195c8385762e9b7b58b
Student <kamil.kielbasa13@gmail.com>
2019-03-11 14:43:31 +0100

    sadsa

--- pierwszy_program.c
+++ pierwszy_program.c
@@ -4,10 +4,69 @@
 #include <util/delay.h>
 #include <avr/io.h>
 
+#define START 0
+#define ERROR() {LCD_WriteText("ERROR");LCD_WriteData((unsigned char)__LINE__[0]);}
+#define SLA_W 0
+#define DATA 0
+#define MT_SLA_ACK 0
+#define MT_DATA_ACK 0
+
+void delay_ms( int ms)
+	{
+	volatile long unsigned int i;
+	for(i=0;i<ms;i++)
+		_delay_ms(1);
+	}
+
 int main(void)
 {
+	
+	int i = 0;
 	LCD_Initalize();
 	//LCD_WriteData('a');
-	LCD_WriteText("juhu");
+	ERROR();
+	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
+	if ((TWSR & 0xF8) != START) ERROR();
+	
+	
+	
+	TWDR = SLA_W;
+	TWCR = (1<<TWINT) | (1<<TWEN);
+
+	while (!(TWCR & (1<<TWINT)));
+
+	if ((TWSR & 0xF8) != MT_SLA_ACK) ERROR();
+	TWDR = DATA;
+	TWCR = (1<<TWINT) | (1<<TWEN);
+
+	while (!(TWCR & (1<<TWINT)));
+
+	if ((TWSR & 0xF8) != MT_DATA_ACK) ERROR();
+
+	TWCR = (1<<TWINT)|(1<<TWEN)| (1<<TWSTO);
+	for(;;)
+	{
+		char data = i % 26 + 65;
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		LCD_WriteData(data);
+		delay_ms(1000);
+		LCD_Clear();
+		i++;
+	}
 	return 0;
 }
